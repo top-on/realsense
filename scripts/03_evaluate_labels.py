f8984a10438655965ae862ed1ee0c3d7acf7e70f
Thorben Jensen <jensen.thorben@gmail.com>
2019-07-06 14:13:38 +0200

    parsing boxes from XML

--- scripts/03_evaluate_labels.py
+++ scripts/03_evaluate_labels.py
@@ -1,7 +1,9 @@
 """ Evaluate labelled data """
-
+# %%
 import os
+from collections import namedtuple
 from pathlib import Path
+from typing import Tuple
 
 from bs4 import BeautifulSoup
 
@@ -15,11 +17,31 @@ img_ids = [Path(i).stem for i in label_files]
 img_files = [Path(DIR_IMAGES + i).with_suffix(".jpg").__str__() for i in img_ids]
 arr_files = [Path(DIR_ARRAYS + i).with_suffix(".npy").__str__() for i in img_ids]
 
-with open(label_files[0]) as fd:
-    xml = fd.read()
-    
-soup = BeautifulSoup(xml, "xml")
-names = [i.find('name').text for i in soup.find_all('object')]
-soup.find_all('object')[0].find('bndbox').__dict__
 
-# TODO parse XML
+def parse_object_to_box(tag) -> Tuple[int, int, int, int]:
+    Box = namedtuple("Box", "xmin xmax ymin ymax")
+    xmin = int(tag.find("xmin").text)
+    xmax = int(tag.find("xmax").text)
+    ymin = int(tag.find("ymin").text)
+    ymax = int(tag.find("ymax").text)
+    _box = Box(xmin, xmax, ymin, ymax)
+    return _box
+
+
+def parse_xml_to_boxes(
+    xml: str
+) -> Tuple[Tuple[int, int, int, int], Tuple[int, int, int, int]]:
+    soup = BeautifulSoup(xml, "xml")
+    obj_low = soup.find("name", string="low").parent
+    obj_high = soup.find("name", string="high").parent
+    box_low = parse_object_to_box(obj_low)
+    box_high = parse_object_to_box(obj_high)
+    return box_low, box_high
+
+
+# %%
+with open(label_files[0]) as fd:
+    text = fd.read()
+
+boxes = parse_xml_to_boxes(xml=text)
+# %%
