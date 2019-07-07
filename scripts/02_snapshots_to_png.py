4d5fadc62119363d94534fa7fe30b2a0336f7df0
Thorben Jensen <jensen.thorben@gmail.com>
2019-07-07 20:31:44 +0200

    leaning 'numpy to png' script

--- scripts/02_snapshots_to_png.py
+++ scripts/02_snapshots_to_png.py
@@ -1,6 +1,10 @@
+""" Convert arrays with depth information to images. """
+# %%
 import os
 from pathlib import Path
+from typing import Dict, Tuple
 
+import cv2 as cv
 import numpy as np
 import png
 
@@ -9,28 +13,38 @@ DIR_OUT = "data/snapshots/png/"
 
 MAX_DIST = 0.5
 
-npy_files = [i for i in os.listdir(DIR_IN) if i.endswith(".npy")]
+npy_files = [DIR_IN + i for i in os.listdir(DIR_IN) if i.endswith(".npy")]
 
-for file in npy_files:
-    arr = np.load(DIR_IN + file)
 
-    depth_center = arr[int(arr.shape[0] / 2), int(arr.shape[1] / 2)]
-    depth_min = depth_center - 0.05
-    depth_max = depth_center + 0.05
-    # clip high and low valuess
-    arr[arr > depth_max] = depth_max
-    arr[arr < depth_min] = depth_min
-    # normalize to [0, 255]
-    arr_trans = ((arr - depth_min) / (depth_max - depth_min) * 65535).astype(np.uint16)
-    # write to file
-    file_out = Path(DIR_OUT + file).with_suffix(".png")
+# %%
+def save_16bit_png(array: Dict[Tuple[int, int], float], file_path: str):
+    # normalize to [0, 1]
+    arr_norm = (array - np.min(array)) / np.ptp(array)
+    # 'strech' to [0, 2**16]
+    arr_16bit = (arr_norm * 2**16).astype(np.uint16)
     # write as 16 bit PNG
-    with open(file_out, "wb") as f:
+    with open(file_path, "wb") as f:
         writer = png.Writer(
-            width=arr_trans.shape[1],
-            height=arr_trans.shape[0],
+            width=arr_16bit.shape[1],
+            height=arr_16bit.shape[0],
             bitdepth=16,
             greyscale=True,
         )
-        zgray2list = arr_trans
-        writer.write(f, zgray2list)
+        writer.write(f, arr_16bit)
+
+
+# %%
+# npy_file = npy_files[1]
+for npy_file in npy_files:
+    arr = np.load(npy_file)
+    # clip high and low valuess
+    depth_center = arr[int(arr.shape[0] / 2), int(arr.shape[1] / 2)]
+    depth_min = max(depth_center - 0.05, 0.01)  # closer than center, but greater 0
+    depth_max = depth_center + 0.1
+    arr[arr > depth_max] = depth_max
+    arr[arr < depth_min] = depth_min
+    # write to file
+    png_file = DIR_OUT + Path(npy_file).stem + ".png"
+    save_16bit_png(array=arr, file_path=png_file)
+
+# %%
